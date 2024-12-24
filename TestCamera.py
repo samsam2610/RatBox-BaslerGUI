35067f3c9119bc836b6315ce15f7bbf6a35bbe00
Sam Tran <quocsam93@gmail.com>
2024-12-24 15:23:30 -0500

    pylon streaming

--- TestCamera.py
+++ TestCamera.py
@@ -103,7 +103,7 @@ def main():
         while camera.IsGrabbing():
             grab_result = camera.RetrieveResult(5000, pylon.TimeoutHandling_ThrowException)
             if grab_result.GrabSucceeded():
-                frame = grab_result.Array  # NumPy array of the image
+                frame = grab_result.GetArray()  # NumPy array of the image
                 timestamp = time.time()
                 frame_number = grab_result.BlockID
 
