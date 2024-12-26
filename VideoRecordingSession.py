c793ca0e3c654671ebd163446550b7c4e679cc28
Sam Tran <quocsam93@gmail.com>
2024-12-26 13:17:40 -0500

    adding precise sleep, remove preview thread

--- VideoRecordingSession.py
+++ VideoRecordingSession.py
@@ -63,7 +63,8 @@ class VideoRecordingSession:
                 buffer_len = len(self.frame_buffer)
                 if buffer_len:
                     self._write_frame()
-            time.sleep(0.001)
+                else:
+                    time.sleep(0.001)
         
         print(f"Cam {self.cam_num}: Frame processing stopped")
         
