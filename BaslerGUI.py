2396d63de4556fa80df291bf1d73ab46e486609f
Sam Tran <quocsam93@gmail.com>
2024-12-30 14:50:10 -0500

    revamping the roi

--- BaslerGUI.py
+++ BaslerGUI.py
@@ -969,7 +969,7 @@ class BaslerGuiWindow(wx.Frame):
     def preview_thread(self):
         # Create Image Windows to display live video while capturing
         imageWindow = pylon.PylonImageWindow()
-        imageWindow.Create(1, nWidth=640, nHeight=480)
+        imageWindow.Create(1, 0, 0, 640, 480)
         
         self.camera.StartGrabbing(pylon.GrabStrategy_OneByOne)
         self.previous_time = int(round(time.time() * 1000))
