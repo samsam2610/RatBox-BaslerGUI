734d75a9b58b11a3fa3a2a815a322a268c1fd07f
Sam Tran <quocsam93@gmail.com>
2025-10-27 14:56:45 -0400

    test

--- src/BaslerGUI.py
+++ src/BaslerGUI.py
@@ -225,27 +225,27 @@ class BaslerGuiWindow(wx.Frame):
                   flag=wx.EXPAND | wx.ALL, border=5)
 
         self.framerate_ctrl_label = wx.StaticText(panel, label="Framerate (Hz):")
-        sizer.Add(self.framerate_ctrl_label, pos=(4, 0), span=(1, 1),
+        sizer.Add(self.framerate_ctrl_label, pos=(8, 0), span=(1, 1),
                   flag=wx.EXPAND | wx.ALL, border=5)
         self.framerate_slider = FS.FloatSpin(panel, -1,  min_val=100, max_val=500,
                                              size=(140, -1), increment=1.0, digits=0,
                                              value=0.1, agwStyle=FS.FS_LEFT)
         self.framerate_slider.Bind(FS.EVT_FLOATSPIN, self.FramerteSliderScroll)
-        sizer.Add(self.framerate_slider, pos=(4, 1), span=(1, 1),
+        sizer.Add(self.framerate_slider, pos=(8, 1), span=(1, 1),
                   flag=wx.ALL, border=5)
 
         self.exposure_ctrl_label = wx.StaticText(panel, label="Exposure (us):")
-        sizer.Add(self.exposure_ctrl_label, pos=(5, 0),  span=(1, 1),
+        sizer.Add(self.exposure_ctrl_label, pos=(9, 0),  span=(1, 1),
                   flag=wx.EXPAND | wx.ALL, border=5)
         self.exposure_slider = FS.FloatSpin(panel, -1,  min_val=1000, max_val=5000,
                                             size=(140, -1), increment=100, digits=0,
                                             value=1000, agwStyle=FS.FS_LEFT)
         self.exposure_slider.Bind(FS.EVT_FLOATSPIN, self.ExposureSliderScroll)
-        sizer.Add(self.exposure_slider, pos=(5, 1), span=(1, 1),
+        sizer.Add(self.exposure_slider, pos=(9, 1), span=(1, 1),
                   flag=wx.ALL, border=5)
 
         self.gain_ctrl_label = wx.StaticText(panel, label="Gain (dB):")
-        sizer.Add(self.gain_ctrl_label, pos=(6, 0), span=(1, 1),
+        sizer.Add(self.gain_ctrl_label, pos=(10, 0), span=(1, 1),
                   flag=wx.EXPAND | wx.ALL, border=5)
         self.gain_slider = FS.FloatSpin(panel, -1,  min_val=0, max_val=1,
                                         size=(140, -1), increment=0.01, value=0.1, digits=0,
@@ -253,51 +253,51 @@ class BaslerGuiWindow(wx.Frame):
         self.gain_slider.Bind(FS.EVT_FLOATSPIN, self.GainSliderScroll)
         self.gain_slider.SetFormat("%f")
         self.gain_slider.SetDigits(3)
-        sizer.Add(self.gain_slider, pos=(6, 1), span=(1, 1),
+        sizer.Add(self.gain_slider, pos=(10, 1), span=(1, 1),
                   flag=wx.ALL, border=5)
 
         self.set_auto_exposure = wx.CheckBox(panel, label="Auto Exposure")
-        sizer.Add(self.set_auto_exposure, pos=(7, 0), span=(1, 1),
+        sizer.Add(self.set_auto_exposure, pos=(11, 0), span=(1, 1),
                   flag=wx.EXPAND | wx.ALL, border=5)
         self.set_auto_exposure.SetBackgroundColour(wx.NullColour)
         self.set_auto_exposure.Bind(wx.EVT_CHECKBOX, self.OnEnableAutoExposure)
 
         self.set_auto_gain = wx.CheckBox(panel, label="Auto Gain")
-        sizer.Add(self.set_auto_gain, pos=(7, 1), span=(1, 1),
+        sizer.Add(self.set_auto_gain, pos=(11, 1), span=(1, 1),
                   flag=wx.EXPAND | wx.ALL, border=5)
         self.set_auto_gain.SetBackgroundColour(wx.NullColour)
         self.set_auto_gain.Bind(wx.EVT_CHECKBOX, self.OnEnableAutoGain)
 
         exportfile_ctrl_label = wx.StaticText(panel, label="Export file name:")
-        sizer.Add(exportfile_ctrl_label, pos=(12, 0), span=(1, 1),
+        sizer.Add(exportfile_ctrl_label, pos=(16, 0), span=(1, 1),
                   flag=wx.EXPAND | wx.ALL, border=5)
         self.exportfile_ctrl = wx.TextCtrl(panel)
-        sizer.Add(self.exportfile_ctrl, pos=(12, 1), span=(1, 1),
+        sizer.Add(self.exportfile_ctrl, pos=(16, 1), span=(1, 1),
                   flag=wx.EXPAND | wx.ALL, border=5)
 
         exportfolder_ctrl_label = wx.StaticText(panel, label="Export directory:")
-        sizer.Add(exportfolder_ctrl_label, pos=(13, 0), span=(1, 1),
+        sizer.Add(exportfolder_ctrl_label, pos=(17, 0), span=(1, 1),
                   flag=wx.EXPAND | wx.ALL, border=5)
 
         self.select_folder_btn = wx.Button(panel, label="Select folder")
         self.select_folder_btn.Bind(wx.EVT_BUTTON, self.OnSelectFolder)
-        sizer.Add(self.select_folder_btn, pos=(13, 1),
+        sizer.Add(self.select_folder_btn, pos=(17, 1),
                   flag=wx.EXPAND | wx.ALL, border=5)
 
         self.exportfolder_ctrl = wx.TextCtrl(panel)
-        sizer.Add(self.exportfolder_ctrl, pos=(14, 0), span=(1, 2),
+        sizer.Add(self.exportfolder_ctrl, pos=(18, 0), span=(1, 2),
                   flag=wx.EXPAND | wx.ALL, border=5)
         self.exportfolder_ctrl.Disable()
 
         self.append_date = wx.CheckBox(panel, label="Append date and time")
-        sizer.Add(self.append_date, pos=(15, 0), span=(1, 1),
+        sizer.Add(self.append_date, pos=(19, 0), span=(1, 1),
                   flag=wx.EXPAND | wx.ALL, border=5)
         self.append_date.SetBackgroundColour(wx.NullColour)
         self.append_date.Bind(wx.EVT_CHECKBOX, self.OnAppendDate)
         self.append_date.SetValue(True)  
 
         self.auto_index = wx.CheckBox(panel, label="Auto index")
-        sizer.Add(self.auto_index, pos=(16, 0), span=(1, 1),
+        sizer.Add(self.auto_index, pos=(20, 0), span=(1, 1),
                   flag=wx.EXPAND | wx.ALL, border=5)
         self.auto_index.SetBackgroundColour(wx.NullColour)
         self.auto_index.Bind(wx.EVT_CHECKBOX, self.OnAutoIndex)
@@ -305,56 +305,56 @@ class BaslerGuiWindow(wx.Frame):
 
         self.index_ctrl = wx.TextCtrl(panel)
         self.index_ctrl.SetValue(str(1))
-        sizer.Add(self.index_ctrl, pos=(16, 1), flag=wx.EXPAND | wx.ALL, border=5)
+        sizer.Add(self.index_ctrl, pos=(20, 1), flag=wx.EXPAND | wx.ALL, border=5)
         
         self.set_roi = wx.CheckBox(panel, label="Set ROI")
-        sizer.Add(self.set_roi, pos=(15, 3), span=(1, 1),
+        sizer.Add(self.set_roi, pos=(19, 3), span=(1, 1),
                   flag=wx.EXPAND | wx.ALL, border=5)
         self.set_roi.SetBackgroundColour(wx.NullColour)
         self.set_roi.Bind(wx.EVT_CHECKBOX, self.OnEnableRoi)
 
         offset_x_ctrl_label = wx.StaticText(panel, label="Offset X:")
-        sizer.Add(offset_x_ctrl_label, pos=(8, 0), span=(1, 1),
+        sizer.Add(offset_x_ctrl_label, pos=(12, 0), span=(1, 1),
                   flag=wx.EXPAND | wx.ALL, border=0)
         self.offset_x_ctrl = FS.FloatSpin(panel, -1,  min_val=0, max_val=self.frame_width,
                                           size=(140, -1), increment=0.1, value=0.1, digits=0,
                                           agwStyle=FS.FS_LEFT)
-        sizer.Add(self.offset_x_ctrl, pos=(8, 1), span=(1, 1),
+        sizer.Add(self.offset_x_ctrl, pos=(12, 1), span=(1, 1),
                   flag=wx.EXPAND | wx.ALL, border=0)
         self.offset_x_ctrl.Bind(FS.EVT_FLOATSPIN, self.OnSetOffsetX)
 
         offset_y_ctrl_label = wx.StaticText(panel, label="Offset Y:")
-        sizer.Add(offset_y_ctrl_label, pos=(9, 0), span=(1, 1),
+        sizer.Add(offset_y_ctrl_label, pos=(13, 0), span=(1, 1),
                   flag=wx.EXPAND | wx.ALL, border=0)
         self.offset_y_ctrl = FS.FloatSpin(panel, -1,  min_val=0, max_val=self.frame_height,
                                     size=(140, -1), increment=0.1, value=0.1, digits=0,
                                     agwStyle=FS.FS_LEFT)
-        sizer.Add(self.offset_y_ctrl, pos=(9, 1), span=(1, 1),
+        sizer.Add(self.offset_y_ctrl, pos=(13, 1), span=(1, 1),
                   flag=wx.EXPAND | wx.ALL, border=0)
         self.offset_y_ctrl.Bind(FS.EVT_FLOATSPIN, self.OnSetOffsetY)
 
         width_ctrl_label = wx.StaticText(panel, label="Width:")
-        sizer.Add(width_ctrl_label, pos=(10, 0), span=(1, 1),
+        sizer.Add(width_ctrl_label, pos=(14, 0), span=(1, 1),
                   flag=wx.EXPAND | wx.ALL, border=0)
         self.width_ctrl = FS.FloatSpin(panel, -1,  min_val=128, max_val=self.frame_width,
                                         size=(140, -1), increment=4, value=128, digits=0,
                                         agwStyle=FS.FS_LEFT)
-        sizer.Add(self.width_ctrl, pos=(10, 1), span=(1, 1),
+        sizer.Add(self.width_ctrl, pos=(14, 1), span=(1, 1),
                   flag=wx.EXPAND | wx.ALL, border=0)
         self.width_ctrl.Bind(FS.EVT_FLOATSPIN, self.OnSetWidth)
 
         height_ctrl_label = wx.StaticText(panel, label="Height:")
-        sizer.Add(height_ctrl_label, pos=(11, 0), span=(1, 1),
+        sizer.Add(height_ctrl_label, pos=(15, 0), span=(1, 1),
                   flag=wx.EXPAND | wx.ALL, border=0)
         self.height_ctrl = FS.FloatSpin(panel, -1,  min_val=128, max_val=self.frame_height,
                                         size=(140, -1), increment=4, value=128, digits=0,
                                         agwStyle=FS.FS_LEFT)
-        sizer.Add(self.height_ctrl, pos=(11, 1), span=(1, 1),
+        sizer.Add(self.height_ctrl, pos=(15, 1), span=(1, 1),
                   flag=wx.EXPAND | wx.ALL, border=0)
         self.height_ctrl.Bind(FS.EVT_FLOATSPIN, self.OnSetHeight)
 
         self.current_state = wx.StaticText(panel, label="Current state: idle")
-        sizer.Add(self.current_state, pos=(22, 0), span=(1, 2),
+        sizer.Add(self.current_state, pos=(26, 0), span=(1, 2),
                   flag=wx.EXPAND | wx.ALL, border=5)
 
         self.offset_x_ctrl.Disable()
@@ -364,10 +364,10 @@ class BaslerGuiWindow(wx.Frame):
         
         # Text field to enter the queue
         self.note_label = wx.StaticText(panel, label="Notes (Press Enter to send):")
-        sizer.Add(self.note_label, pos=(23, 0), span=(1, 1),
+        sizer.Add(self.note_label, pos=(27, 0), span=(1, 1),
                   flag=wx.EXPAND | wx.ALL, border=0)
         self.note_ctrl = wx.TextCtrl(panel, style=wx.TE_PROCESS_ENTER)
-        sizer.Add(self.note_ctrl, pos=(23, 1), span=(1, 2),
+        sizer.Add(self.note_ctrl, pos=(27, 1), span=(1, 2),
                   flag=wx.EXPAND | wx.ALL, border=5)
 
         self.next_note_q = queue.Queue(maxsize=1)
