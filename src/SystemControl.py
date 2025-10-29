c3dfc34981a86987f0721a19cb42aa31c354faef
Sam Tran <quocsam93@gmail.com>
2025-10-29 15:19:46 -0400

    test

--- src/SystemControl.py
+++ src/SystemControl.py
@@ -18,7 +18,7 @@ from ImagePanel import ImagePanel
 from CameraController import CameraController
 
 class SystemControl(wx.Frame):
-    def __init__(self, parent, number_of_cameras=2, *args, **kwargs):
+    def __init__(self, number_of_cameras=2, *args, **kwargs):
         super(SystemControl, self).__init__(*args, **kwargs)
         self.number_of_cameras = number_of_cameras
         if self.number_of_cameras > 1:
@@ -40,5 +40,5 @@ class SystemControl(wx.Frame):
 
 if __name__ == '__main__':
     app = wx.App()
-    ex = SystemControl(None, number_of_cameras=1)
+    ex = SystemControl(number_of_cameras=1)
     app.MainLoop()
