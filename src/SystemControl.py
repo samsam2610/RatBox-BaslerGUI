645ff896fdac4295d9d0d0cd1cb8627391c2d62d
Sam Tran <quocsam93@gmail.com>
2025-10-30 13:56:27 -0400

    test multicam

--- src/SystemControl.py
+++ src/SystemControl.py
@@ -23,6 +23,8 @@ class SystemControl(wx.Frame):
         # Outer panel so we can attach a sizer
         self.outer_panel = wx.Panel(self)
         
+        self.camera_panels = []
+        
         self.number_of_cameras = number_of_cameras
         if self.number_of_cameras > 1:
             print("Multi-camera mode activated.")
@@ -56,7 +58,25 @@ class SystemControl(wx.Frame):
 
             # Add to the main horizontal layout
             hbox.Add(static_sizer, proportion=1, flag=wx.EXPAND | wx.ALL, border=5)
-        
+
+            self.camera_panels.append(self.camera_panel)
+        else:
+            for i in range(self.number_of_cameras):
+                print(f"Initializing camera {i + 1} UI...")
+                camera_panel = CameraController(self.outer_panel, cam_index=i, cam_details=f"Camera {i + 1}", multi_cam=True, column_pos=i, row_pos=0)
+                camera_panel.InitUI()
+                self.SetTitle(f"Multi-Camera Control - {self.number_of_cameras} Cameras")
+
+                # Put each camera panel in a StaticBoxSizer for visual grouping
+                box = wx.StaticBox(self.outer_panel, label=f"Camera {i + 1} Controls")
+                static_sizer = wx.StaticBoxSizer(box, wx.VERTICAL)
+                static_sizer.Add(camera_panel, proportion=1, flag=wx.EXPAND | wx.ALL, border=5)
+
+                # Add to the main horizontal layout
+                hbox.Add(static_sizer, proportion=1, flag=wx.EXPAND | wx.ALL, border=5)
+
+                self.camera_panels.append(camera_panel)
+
         self.outer_panel.SetSizer(hbox)
         hbox.Layout()
         
@@ -69,5 +89,5 @@ class SystemControl(wx.Frame):
 
 if __name__ == '__main__':
     app = wx.App()
-    ex = SystemControl(number_of_cameras=1)
+    ex = SystemControl(number_of_cameras=2)
     app.MainLoop()
