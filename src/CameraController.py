c988574916365678040211f1897355b0d5024444
Sam Tran <quocsam93@gmail.com>
2025-12-09 15:20:54 -0500

    test barrier

--- src/CameraController.py
+++ src/CameraController.py
@@ -1417,7 +1417,6 @@ class CameraController(wx.Panel):
             # Check frame_count_sync to see if all the other cameras have captured the same number of frames, if not, wait at the barrier
             if self.barrier is not None:
                 # If other cameras are behind, wait at the barrier:
-                # while captured_frames > min(self.frame_count_sync) and self.calibration_on is True:
                 try:
                     # time.sleep(0.0001)  # small sleep to allow other threads to catch up
                     self.barrier.wait()
@@ -1428,10 +1427,9 @@ class CameraController(wx.Panel):
             try:
                 grabResult = self.camera.RetrieveResult(100, pylon.TimeoutHandling_ThrowException)
             except pylon.TimeoutException:
-                # print("Timeout occurred while waiting for a frame.")
+                print("Timeout occurred while waiting for a frame.")
                 continue
 
-            
             if self.camera.NumReadyBuffers.GetValue() > 0:
                 print(f"Frames in buffer: {self.camera.NumReadyBuffers.GetValue()}")
 
